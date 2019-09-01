e81214fd919c0df079af4d0e57795b19ff0cce45
Li Xueli <lixueli93@gmail.com>
2019-09-01 10:52:21 +0800

    add lint and format

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -5,20 +5,20 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require('react');
+const React = require('react')
 
 class Footer extends React.Component {
   docUrl(doc, language) {
-    const baseUrl = this.props.config.baseUrl;
-    const docsUrl = this.props.config.docsUrl;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    return `${baseUrl}${docsPart}${langPart}${doc}`;
+    const { baseUrl } = this.props.config
+    const { docsUrl } = this.props.config
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`
+    const langPart = `${language ? `${language}/` : ''}`
+    return `${baseUrl}${docsPart}${langPart}${doc}`
   }
 
   pageUrl(doc, language) {
-    const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language ? `${language}/` : '') + doc;
+    const { baseUrl } = this.props.config
+    return baseUrl + (language ? `${language}/` : '') + doc
   }
 
   render() {
@@ -37,32 +37,18 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
-            </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
-            </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
-            </a>
+            <a href={this.docUrl('doc1.html', this.props.language)}>Getting Started (or other categories)</a>
+            <a href={this.docUrl('doc2.html', this.props.language)}>Guides (or other categories)</a>
+            <a href={this.docUrl('doc3.html', this.props.language)}>API Reference (or other categories)</a>
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
-            </a>
-            <a
-              href="https://stackoverflow.com/questions/tagged/"
-              target="_blank"
-              rel="noreferrer noopener">
+            <a href={this.pageUrl('users.html', this.props.language)}>User Showcase</a>
+            <a href="https://stackoverflow.com/questions/tagged/" target="_blank" rel="noreferrer noopener">
               Stack Overflow
             </a>
             <a href="https://discordapp.com/">Project Chat</a>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noreferrer noopener">
+            <a href="https://twitter.com/" target="_blank" rel="noreferrer noopener">
               Twitter
             </a>
           </div>
@@ -77,16 +63,13 @@ class Footer extends React.Component {
               data-count-href="/facebook/docusaurus/stargazers"
               data-show-count="true"
               data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
+              aria-label="Star this project on GitHub"
+            >
               Star
             </a>
             {this.props.config.twitterUsername && (
               <div className="social">
-                <a
-                  href={`https://twitter.com/${
-                    this.props.config.twitterUsername
-                  }`}
-                  className="twitter-follow-button">
+                <a href={`https://twitter.com/${this.props.config.twitterUsername}`} className="twitter-follow-button">
                   Follow @{this.props.config.twitterUsername}
                 </a>
               </div>
@@ -107,11 +90,7 @@ class Footer extends React.Component {
           </div>
         </section>
 
-        <a
-          href="https://opensource.facebook.com/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="fbOpenSource">
+        <a href="https://opensource.facebook.com/" target="_blank" rel="noreferrer noopener" className="fbOpenSource">
           <img
             src={`${this.props.config.baseUrl}img/oss_logo.png`}
             alt="Facebook Open Source"
@@ -121,8 +100,8 @@ class Footer extends React.Component {
         </a>
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
-    );
+    )
   }
 }
 
-module.exports = Footer;
+module.exports = Footer
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -9,47 +9,50 @@
 // site configuration options.
 
 // List of projects/orgs using your project for the users page.
-const users = [{
-  caption: 'User1',
-  // You will need to prepend the image path with your baseUrl
-  // if it is not '/', like: '/test-site/img/image.jpg'.
-  image: '/img/undraw_open_source.svg',
-  infoLink: 'https://www.facebook.com',
-  pinned: true,
-}, ];
+const users = [
+  {
+    caption: 'User1',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/undraw_open_source.svg',
+    infoLink: 'https://www.facebook.com',
+    pinned: true,
+  },
+]
 
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  url: 'https://recipe.xueli.li', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'chaos-recipe-dist',
+  organizationName: 'mixj93',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [{
+  headerLinks: [
+    {
       doc: 'doc1',
-      label: 'Docs'
+      label: 'Docs',
     },
     {
       doc: 'doc4',
-      label: 'API'
+      label: 'API',
     },
     {
       page: 'help',
-      label: 'Help'
+      label: 'Help',
     },
     {
       blog: true,
-      label: 'Blog'
+      label: 'Blog',
     },
   ],
 
@@ -114,6 +117,6 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
-};
+}
 
-module.exports = siteConfig;
+module.exports = siteConfig
